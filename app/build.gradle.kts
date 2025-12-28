7cf9547b1897ef3e99111adc02c0b538453dd196
Berke Balci <berkebalcii38@gmail.com>
2025-12-28 15:52:55 +0100

    Implemented test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,4 +83,5 @@ dependencies {
     // Testing
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    testImplementation(kotlin("test"))
 }
--- app/src/androidTest/kotlin/ItemDaoTest.kt
+++ app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -1,2 +1,61 @@
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.runner.AndroidJUnit4
+import com.example.inventory.data.InventoryDataBase
+import com.example.inventory.data.Item
+import org.junit.runner.RunWith
+import com.example.inventory.data.ItemDAO
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
 class ItemDaoTest {
+    private lateinit var itemDao: ItemDAO
+    private lateinit var inventoryDatabase: InventoryDataBase
+
+    @Before
+    fun createDb(){
+        val context: Context = ApplicationProvider.getApplicationContext()
+        inventoryDatabase = Room.inMemoryDatabaseBuilder(
+            context,
+            InventoryDataBase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+        itemDao = inventoryDatabase.itemDao()
+    }
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        inventoryDatabase.close()
+    }
+
+
+
+    private var item1 = Item(1, "Apples", 10.0, 20)
+    private var item2 = Item(2, "Bananas", 15.0, 97)
+
+    private suspend fun addOneItemToDb() {
+        itemDao.insert(item1)
+    }
+
+    private suspend fun addTwoItemsToDb() {
+        itemDao.insert(item1)
+        itemDao.insert(item2)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun daoInsert_insertsItemIntoDB() = runBlocking {
+        addOneItemToDb()
+        val allItems = itemDao.getAllItems().first()
+        assertEquals(allItems[0], item1)
+    }
+
 }
